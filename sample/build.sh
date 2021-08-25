533ee0f6797f9a582ce940c1bd21f71d9cf97c5c
wngr <oliver@wngr.de>
2021-08-25 19:41:57 +0200

    add sample

---
+++ sample/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+
+RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
+  cargo build --target wasm32-unknown-unknown --release -Z build-std=std,panic_abort
+
+wasm-bindgen \
+  ./target/wasm32-unknown-unknown/release/sample.wasm \
+  --out-dir . \
+  --target web \
+  --weak-refs
--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,10 @@
-use std::time::Duration;
-
-use futures::channel::mpsc;
-use futures::StreamExt;
-use instant::Instant;
-use js_sys::Promise;
-use log::*;
-use wasm_bindgen::{prelude::*, JsCast};
-use web_sys::DedicatedWorkerGlobalScope;
-
-use self::pool::WorkerPool;
-use self::thread_pool::ThreadPool;
-
+///! TODO
 mod pool;
-mod thread_pool;
 mod unpark_mutex;
+
+pub use self::pool::ThreadPool;
 // Note: `atomics` is whitelisted in `target_feature` detection, but `bulk-memory` isn't,
 // so we can check only presence of the former. This should be enough to catch most common
 // mistake (forgetting to pass `RUSTFLAGS` altogether).
 #[cfg(not(target_feature = "atomics"))]
 compile_error!("Did you forget to enable `atomics` and `bulk-memory` features as outlined in wasm-bindgen-rayon README?");
-
-#[wasm_bindgen(start)]
-pub fn main() {
-    let _ = console_log::init_with_level(log::Level::Info);
-    ::console_error_panic_hook::set_once();
-}
-
-#[wasm_bindgen]
-pub fn start_async() -> Promise {
-    let pool_size = web_sys::window()
-        .unwrap()
-        .navigator()
-        .hardware_concurrency() as usize;
-    let pool = ThreadPool::new(pool_size).unwrap();
-    let (tx, mut rx) = mpsc::channel(10);
-    for i in 0..1024 {
-        let mut tx_c = tx.clone();
-        pool.spawn_ok(async move {
-            let global = js_sys::global().unchecked_into::<DedicatedWorkerGlobalScope>();
-            info!("Task {} running on {}", i, global.name());
-            let now = Instant::now();
-            //while now.elapsed() < Duration::from_secs(5) {}
-            tx_c.start_send(i * i).unwrap();
-        });
-    }
-    wasm_bindgen_futures::future_to_promise(async move {
-        let mut i = 0;
-        while let Some(x) = rx.next().await {
-            i += x;
-        }
-        Ok(i.into())
-    })
-}
