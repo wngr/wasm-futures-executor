10314e6d75c321a8c52e10ecef761612db1c46dd
wngr <oliver@wngr.de>
2021-08-25 22:17:01 +0200

    add github actions

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 ///! environment. Instead of using spawning threads via `std::thread`, web
 ///! workers are created. This crate tries hard to make this process as
 ///! seamless and painless as possible.
-///! 
+///!
 ///! For further information and usage examples please check the [repository].
-///! 
+///!
 ///! [`futures_executor::ThreadPool`]: https://docs.rs/futures-executor/0.3.16/futures_executor/struct.ThreadPool.html
 ///! [repository]: https://github.com/wngr/wasm-futures-executor
 mod pool;
